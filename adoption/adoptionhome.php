609f397acf4c2009dfebb4cf2c21be69e57c3657
dolon13 <47296774+dolon13@users.noreply.github.com>
2022-09-04 01:57:40 +0600

    Adoption page dynamic

--- adoption/adoptionhome.php
+++ adoption/adoptionhome.php
@@ -1,4 +1,5 @@
 <?php include "adoptionheader.php";
+    include "config.php";
     function getBtnName()
     {
         return 'Registration';   
@@ -7,6 +8,7 @@
     {
         return '../registration.php';
     }
+    
 ?>
 
 <div class="card card-body shadow-xl mx-3 mx-md-4 mt-n6">
@@ -19,10 +21,13 @@
               <div class="col-md-6"> -->
               <!-- ms-auto mt-lg-0 mt-4 -->
         <?php 
-            for($i = 0; $i <2; $i++) {
+            $sql = "SELECT * FROM pet_info";
+            $result = mysqli_query($conn,$sql);
+
+            while($row = mysqli_fetch_assoc($result)) {
                 ?>
                 <br>
-          <div class="col-lg-4">
+          <div class="col-lg-4 p-4">
             <div class="card">
               <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                 <a class="d-block blur-shadow-image">
@@ -31,13 +36,16 @@
               </div>
               <div class="card-body text-right">
                 <h5 class="font-weight-normal">
-                  <a href="javascript:;"><span  style="color: red;">Nickname:</span> Niccki</a>
+                  <a href="javascript:;"><span  style="color: red;">Nickname:</span> <?php echo $row['pet_name']; ?></a>
                 </h5>
                 <h6 class="font-weight-normal">
-                  <a href="javascript:;"><span  style="color: red;">Age:</span> 2 y</a>
+                  <a href="javascript:;"><span  style="color: red;">Age:</span> <?php echo $row['pet_age']; ?> </a>
                 </h6>
                 <p class="mb-0">
-                  Pets description small
+                <?php echo $row['pet_owner']; ?><br>
+                <?php echo $row['contact_no']; ?> <br>
+                <?php echo $row['address']; ?> <br>
+                <?php echo $row['medical_condition']; ?>
                 </p>
                 <a type="button" href="petinformation.php" class="btn bg-gradient-info btn-sm mb-0 mt-3">Find out more</a>
               </div>
